3ddd1fdee6b04448c4f2a37ada88d911e84d2af6
vladbakhteev <bakhteev_v@mail.ru>
2021-03-04 21:40:01 +0300

    Add cycle translation augmentation

--- data.py
+++ data.py
@@ -3,20 +3,34 @@ from typing import List
 
 import torch
 from torch.utils.data import Dataset, DataLoader
+from googletrans import Translator
 
 
 class TranslationDataset(Dataset):
     def __init__(self, sentences_ru, sentences_en):
         self.sentences_ru = sentences_ru
         self.sentences_en = sentences_en
+        self.translator = Translator()
 
     def __getitem__(self, index):
         src = self.sentences_ru[index]
         target = self.sentences_en[index]
         return src, target
 
+    def augment_ru(self, text):
+        return cycle_augmentation(text, self.translator, src='ru', dest='en')
+
+    def augment_en(self, text):
+        return cycle_augmentation(text, self.translator, src='en', dest='ru')
+
     def __len__(self):
         return len(self.sentences_en)
+
+
+def cycle_augmentation(text, translator, src, dest):
+    translated_text = translator.translate(text, src=src, dest=dest).text
+    retranslated_text = translator.translate(translated_text, src=dest, dest=src).text
+    return retranslated_text
 
 
 def collate_fn_wo_tokenizer(batch, tokenizer):
